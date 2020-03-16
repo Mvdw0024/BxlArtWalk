0835ed3620d8051f0111971d8e932ae86c95da85
Eclipse0024 <m.vdw@live.be>
2020-03-16 10:32:55 +0100

    Initial commit

---
+++ app/src/main/java/michael/vdw/bxlartwalk/MainActivity.java
@@ -0,0 +1,14 @@
+package michael.vdw.bxlartwalk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
