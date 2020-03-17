a2374a20eed70151a0d6a0ec8156fa054b5b33ab
TaliaGD <taliadecremer@telenet.be>
2020-03-17 11:15:06 +0100

    aanmaak van Datail,ArtList,Setting,favorits -Fragment met bijhorende UI

---
+++ app/src/main/java/michael/vdw/bxlartwalk/Fragments/DetailFragment.java
@@ -0,0 +1,39 @@
+package michael.vdw.bxlartwalk.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import michael.vdw.bxlartwalk.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DetailFragment extends Fragment {
+
+    public DetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        TextView titleTv = rootView.findViewById(R.id.tv_detail_titleOfTheArt);
+        ImageView photoIv = rootView.findViewById(R.id.iv_detal_photo);
+        TextView yearTv = rootView.findViewById(R.id.tv_detail_yearOfTheArt);
+        TextView authorTv =rootView.findViewById(R.id.tv_detail_authorOfTheArt);
+
+        //TODO data binnen trekken
+        return rootView;
+    }
+}
---
+++ app/src/main/java/michael/vdw/bxlartwalk/Fragments/FavortitListFragment.java
@@ -0,0 +1,74 @@
+package michael.vdw.bxlartwalk.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import michael.vdw.bxlartwalk.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FavortitListFragment extends Fragment {
+    //TODO search uitschrijven omvat(OnQueryTextListener, onQueryTextSubmit, onQueryTextChange( bevat adapter dus aanmaken en gebruiken))
+//    private SearchView.OnQueryTextListener searchListener = new SearchView.OnQueryTextListener() {
+//        //pas filteren na zoeken
+//        @Override
+//        public boolean onQueryTextSubmit(String query) {
+//            return false;
+//        }
+//        //per letter filteren automatisch
+//        @Override
+//        public boolean onQueryTextChange(String newText) {
+//            adapter .getFilter().filter(newText);
+//            return false;
+//        }
+//    };
+//    private ... adapter;
+
+    public FavortitListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_favortit_list, container, false);
+//        //noodzakelijk omp search in te voegen
+//        setHasOptionsMenu(true);
+
+        //verwijzing UI
+        RecyclerView rvFavorit = rootView.findViewById(R.id.rv_favorit);
+        //opvulling rv
+        rvFavorit.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
+        //TODO adapter, nodig om data om te zetten in iets visueel(in dit geval card)
+
+        //TODO verwijzing naar viewModel, waar staat alle data
+        return rootView;
+    }
+    //TODO menu aanmaken en dan onCreateOptionsMenu
+
+//    @Override
+//    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+//
+//        inflater.inflate(R.menu. ... , menu);
+//
+//        SearchView searchView = (SearchView) menu.findItem(R.id. ... ).getActionView();
+//        searchView.setOnQueryTextListener(searchListener);
+//
+//        super.onCreateOptionsMenu(menu, inflater);
+
+}
