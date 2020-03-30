e65de7edbfe4b968a237bdf1ff234229ce811f03
Eclipse0024 <m.vdw@live.be>
2020-03-30 16:09:43 +0200

    Typeconverter aangepast voor LatLng , Test nodig maar emulator crashed steeds
    Signed-off-by: Eclipse0024 <m.vdw@live.be>

--- app/src/main/java/michael/vdw/bxlartwalk/Utils/Converters.java
+++ app/src/main/java/michael/vdw/bxlartwalk/Utils/Converters.java
@@ -15,7 +15,7 @@ public class Converters {
     public static LatLng fromJSON(Double jsontoLatLng) {
         return (jsontoLatLng == null) ? null : new LatLng(jsontoLatLng,jsontoLatLng);
     }
-
+// aangepast om opnieuw te pushen
     @TypeConverter
     public static Double toLatLng(LatLng latLng) {
         return latLng == null ? null : latLng.latitude;
